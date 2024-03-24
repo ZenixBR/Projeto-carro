49ac65f8b7cc8a2e5b631aba4a7e96634089c9de
Vinicius Viera <viniciusviera301@gmail.com>
2024-03-24 11:16:27 -0300

    Terceiro commit do App

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ export default {
   theme: {
     colors: {
       'orange': '#E28625',
-      'green': '##006971',
+      'green': '#006971',
       'dark-green': '#004140',
-      'offwhite': '#rgba(255, 255, 255, 0.75)',
+      'offwhite': 'rgba(255, 255, 255, 0.75)',
       'white': '#fff',
       'offwhitebg': '#f2f2f2',
+    },
+    fontFamily: {
+      'shouledrs': '"Big Shoulder Display"',
+      'lexend': '"Lexend Deca"',
     }
   },
   plugins: [],
